1891e9a22a2ca6e3c3cdf8f213ff748a3333c106
Nilank Sharma <sharmanilank@gmail.com>
2025-05-10 12:44:49 -0400

    Update path

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -229,7 +229,7 @@ const Index = () => {
               }`}
             >
               <img
-                src={`${import.meta.env.BASE_URL}public/developer.jpeg`}
+                src="/developer.jpeg"
                 alt="Developer portrait"
                 className="w-full h-96 object-cover"
               />
